05df9a7c896c0251cb4d416abe9b49a97913ad83
bandi <pappe@edu.bme.hu>
2025-03-14 10:18:32 +0100

    Lesson 01

--- fenykepalbum/views.py
+++ fenykepalbum/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def hello(request):
+    return HttpResponse("Hello, world.")
+
+from django.shortcuts import render
+def homepage(request):
+    return render(request, 'home.html')
