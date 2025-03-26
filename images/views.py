35b78514df3b01b0ccf9acce11e70e98affe7424
bandi <pappe@edu.bme.hu>
2025-03-26 09:09:44 +0100

    Only logged in users can see the pictures Logout button fixed

--- images/views.py
+++ images/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from .models import Image
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def images_list(request):
     images = Image.objects.all()
     return render(request,  'images/images_list.html', {'images': images})
 
+@login_required
 def image_page(request, slug):
     image = Image.objects.get(slug=slug)
     return render(request,  'images/image_page.html', {'image': image})
--- users/views.py
+++ users/views.py
@@ -26,6 +26,5 @@ def login_view(request):
 
 
 def logout_view(request):
-    if request.method == 'POST':
-        logout(request)
-        return redirect("images:list")
+    logout(request)
+    return redirect("/")
