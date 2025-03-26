3c99cdeab86fb1544c86c65cebec2e2d25598c9c
bandi <pappe@edu.bme.hu>
2025-03-26 10:23:26 +0100

    Better login, register and upload pages

--- felho_fenykepalbum/settings.py
+++ felho_fenykepalbum/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
     'fenykepalbum.apps.FenykepalbumConfig',
     'images',
     'users',
